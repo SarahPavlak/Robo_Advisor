4d7afa78665fa3fc66b581fbc342d4f975b13fb8
SarahPavlak <46604461+SarahPavlak@users.noreply.github.com>
2019-02-22 01:28:29 -0500

    Update robo_advisor.py
    dynamically got latest day close - screencast

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -9,7 +9,6 @@ def to_usd(my_price):
         return "${0:,.2f}".format(my_price) #taken from screencast
 
 API_KEY = os.environ.get('MY_API_KEY')
-print(API_KEY)
 
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=AMZN&apikey="+ API_KEY
 
@@ -19,10 +18,12 @@ response = requests.get(request_url)
 
 #print("Status:" +  str(response.status_code)) #from class
 #print("Response Text: " + response.text) #from class
-
 parsed_response = json.loads(response.text)
-last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
-latest_close_usd = parsed_response["Time Series (Daily)"]["2019-02-20"]["4. close"]
+tsd = parsed_response["Time Series (Daily)"]
+dates = list(tsd.keys())
+latest_day = dates[0]
+last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"] 
+latest_close_usd = tsd[latest_day]["4. close"]
 
 #breakpoint() #from class
 
@@ -31,7 +32,6 @@ parsed_response["Time Series (Daily)"]["2019-02-19"]["4. close"] #type it in the
 symbol = "NFLX" # TODO: capture user input, like... input("Please specify a stock symbol: ")
 
 
-# TODO: further revise the example outputs below to reflect real information
 print("-----------------")
 print(f"STOCK SYMBOL: {symbol}")
 print("RUN AT: 11:52pm on June 5th, 2018") #use date time module
@@ -48,13 +48,5 @@ print("-----------------")
 parsed_response["Meta Data"].keys()
 
 #need to install pip install python-dotenv
-
-# see: https://www.alphavantage.co/documentation/#daily (or a different endpoint, as desired)
-# TODO: assemble the request url to get daily data for the given stock symbol...
-
-# TODO: use the "requests" package to issue a "GET" request to the specified url, and store the JSON response in a variable...
-
-# TODO: traverse the nested response data structure to find the latest closing price and other values of interest...
-
 
 # TODO: write response data to a CSV file
